0fe4df5eb75e02a38fae94f4074c2afdbb116093
yaron <yaron@mmuze.com>
2019-07-27 00:29:48 +0300

    getBadgeClasses

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0
+  };
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold"
   };
   // state = {  }
   render() {
     return (
       <dev>
-        <span className="badge badge-primary m-2"> {this.formatCount()}</span>{" "}
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </dev>
     );
   }
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
