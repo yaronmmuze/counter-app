ab3312111bec8e654f6b83c3a0da0de0e079e7f3
yaron <yaron@mmuze.com>
2019-07-27 15:54:31 +0300

    relying entirely on the props object (no local state)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  handleIncrement = product => {
-    // arrow function inherit 'this'
-    // console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     // console.log("props", this.props);
     return (
@@ -19,7 +9,7 @@ class Counter extends Component {
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           //   onClick={() => this.handleIncrement(product)}
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -35,12 +25,12 @@ class Counter extends Component {
     );
   }
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
