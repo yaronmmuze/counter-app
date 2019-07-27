27c2fbeac8834a6ad729adf074697a782901517c
yaron <yaron@mmuze.com>
2019-07-27 21:17:22 +0300

    object distructuring

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,19 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
         {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete} //pass reference to function handleDelete
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete} //pass reference to function handleDelete
+            onIncrement={onIncrement}
             // value={counter.value}
             // id={counter.id}
             counter={counter} // encaps. contains the two above fields and more field that may be added during dev
